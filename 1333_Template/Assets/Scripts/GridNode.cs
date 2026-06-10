bc98a44d3352657de7e94451036e73528facb3fa
gamblin0 <baraybolat@gmail.com>
2026-06-09 23:48:55 -0700

    explanations for GridNode

--- 1333_Template/Assets/Scripts/GridNode.cs
+++ 1333_Template/Assets/Scripts/GridNode.cs
@@ -8,6 +8,6 @@ public struct GridNode
 
     public string Name; // an index to keep track and organize nodes
     public Vector3 WorldPosition;
-    public bool Walkable => TerrainType != null && TerrainType.Walkable;
-    public int Weight => TerrainType == null ? 1: TerrainType.Weight;
+    public bool Walkable => TerrainType != null && TerrainType.Walkable; //if terraain type isn't null and is assigned walkable in TerrainType, it is walkable
+    public int Weight => TerrainType == null ? 1: TerrainType.Weight; //Defaults weight to 1 if Wight isnt assigned in TerrainType
 }
